bb4705f2605cd01f26b95d98e7d8dae1f1260a32
sergioaguirre13 <sergio.aguirre@bue.edu.ar>
2025-09-03 16:22:20 -0300

    first

---
+++ CRUD.EntityLayer/Departamento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD.EntityLayer
+{
+    public class Departamento
+    {
+        public int idDepartamento {  get; set; }
+        public string Nombre { get;set; }
+    }
+}
