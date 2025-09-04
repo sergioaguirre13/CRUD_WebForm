6136c5f0c7dab5439a27bf5fdc7e6cab9ea296e7
sergioaguirre13 <sergio.aguirre@bue.edu.ar>
2025-09-03 21:41:41 -0300

    second

---
+++ CRUD.DataLayer/DepartamentoDL.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CRUD.EntityLayer;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace CRUD.DataLayer
+{
+    public class DepartamentoDL
+    {
+        public List<Departamento> lista()
+        {
+            List<Departamento> lista = new List<Departamento>();
+
+            using (SqlConnection oConexion = new SqlConnection(Conexion.cadena))
+            {
+                SqlCommand cmd = new SqlCommand("select * from fn_departamentos()", oConexion);
+                cmd.CommandType = CommandType.Text;
+                try
+                {
+                    oConexion.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new Departamento
+                            {
+                                idDepartamento = Convert.ToInt32(dr["idDepartamento"].ToString()),
+                                //nombre
+                            });
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    throw ex;
+                }
+            }
+        }
+
+
+    }
+}
